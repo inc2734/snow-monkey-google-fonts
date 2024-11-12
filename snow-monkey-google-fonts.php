a716f041a20c3d03f8854d8d405e021b77825452
inc2734 <inc@2inc.org>
2024-11-13 07:35:32 +0900

    #4 load_plugin_textdomain() を init から後に変更

--- snow-monkey-google-fonts.php
+++ snow-monkey-google-fonts.php
@@ -38,8 +38,7 @@ class Bootstrap {
 	 * Plugins loaded.
 	 */
 	public function _bootstrap() {
-		load_plugin_textdomain( 'snow-monkey-google-fonts', false, basename( __DIR__ ) . '/languages' );
-
+		add_action( 'init', array( $this, '_load_textdomain' ) );
 		add_action( 'init', array( $this, '_activate_autoupdate' ) );
 
 		$theme = wp_get_theme( get_template() );
@@ -191,6 +190,13 @@ class Bootstrap {
 		);
 
 		return $settings;
+	}
+
+	/**
+	 * Load textdomain
+	 */
+	public function _load_textdomain() {
+		load_plugin_textdomain( 'snow-monkey-google-fonts', false, basename( __DIR__ ) . '/languages' );
 	}
 
 	/**
